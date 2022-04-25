2a2a425bcf43af58b7d01d5da6fb4631a93b8f8d
Guillermo Rodriguez <guillermo@telos.net>
2022-04-25 11:09:46 -0300

    Add missing files for msig, token and wrap testing

---
+++ tests/1_do_upgrade/test_upgrade.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+
+def test_upgrade(cleos):
+
+    # perform protocol activations
+    eos_mainnet = 'https://proton.greymass.com'
+    telos_testnet = 'https://testnet.telos.net'
+
+    feat_diff = cleos.diff_protocol_activations(
+        eos_mainnet, telos_testnet)
+
+    for feat in feat_diff:
+        cleos.activate_feature(feat)
+
+    # deploy new contracts
+    cleos.deploy_contract_from_host(
+        'eosio.msig',
+        'contracts/new/eosio.msig',
+        create_account=False,
+        verify_hash=False
+    )
+    # cleos.deploy_contract_from_host(
+    #     'eosio',
+    #     'contracts/new/eosio.system',
+    #     create_account=False
+    # )
+    cleos.deploy_contract_from_host(
+        'eosio.token',
+        'contracts/new/eosio.token',
+        create_account=False,
+        verify_hash=False
+    )
+    cleos.deploy_contract_from_host(
+        'eosio.wrap',
+        'contracts/new/eosio.wrap',
+        create_account=False,
+        verify_hash=False
+    )
+
